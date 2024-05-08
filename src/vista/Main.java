66da4901966c334c8b7b570eb90800b07b1586fe
AlejandroC23 <alejandro.cevallos919@ist17dejulio.edu.ec>
2024-05-08 14:23:00 -0500

    Tipos de datos

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,10 +4,35 @@
  */
 package vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alejandro
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        //TIPOS DE DATOS
+        //1.- ENTERO
+        //1.1.- DECLARACIÓN CON VALOR
+        int numeroUno = 0;
+        int numeroDos = 0;
+        
+        //1.2.- SOBREESCRIBIR EL VALOR
+        numeroUno = 19;
+        numeroDos = 13;
+        
+        //SUMA 2 NÚMEROS
+        int resultado = numeroUno + numeroDos;
+        
+        //IMPRIMIR SALIDA DE INFORMACIÓN
+        System.out.println("El resultado es: " + resultado);
+        
+        
+        //1.3.- UTILIZACIÓN DE ENTRADAS
+        Scanner s = new Scanner(System.in);
+        
+        //TRANSFORMAR DE DATO PRIMITIVO A OBJETO
+        int num1 = Integer.parseInt(s.next());
+    }
 }
